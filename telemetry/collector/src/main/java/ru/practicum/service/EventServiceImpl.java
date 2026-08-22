7b7e6e0943a9fac4565b1e893a616980872274f3
Artem Ledovskikh <ledartem@bk.ru>
2026-08-22 12:29:24 +0700

    Реализация Collector через GRPC

--- telemetry/collector/src/main/java/ru/practicum/service/EventServiceImpl.java
+++ telemetry/collector/src/main/java/ru/practicum/service/EventServiceImpl.java
@@ -1,35 +1,73 @@
 package ru.practicum.service;
 
+import com.google.protobuf.InvalidProtocolBufferException;
+import com.google.protobuf.util.JsonFormat;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import ru.practicum.mapper.EventMapper;
 import ru.practicum.model.hub.HubEvent;
 import ru.practicum.model.sensor.SensorEvent;
+import ru.practicum.service.handler.hub.HubEventHandler;
+import ru.practicum.service.handler.sensor.SensorEventHandler;
+import ru.yandex.practicum.grpc.telemetry.messages.hub.HubEventProto;
+import ru.yandex.practicum.grpc.telemetry.messages.sensor.SensorEventProto;
 import ru.yandex.practicum.kafka.telemetry.hub.HubEventAvro;
 import ru.yandex.practicum.kafka.telemetry.sensor.SensorEventAvro;
 
+import java.util.Map;
+import java.util.Set;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
 @Service
 @Slf4j
-@RequiredArgsConstructor
 public class EventServiceImpl implements EventService {
-    private final KafkaEventProducer producer;
-    private static final String HUBS_TOPIC = "telemetry.hubs.v1";
-    private static final String SENSORS_TOPIC = "telemetry.sensors.v1";
+    private final Map<SensorEventProto.PayloadCase, SensorEventHandler> sensorEventHandlers;
+    private final Map<HubEventProto.PayloadCase, HubEventHandler> hubEventHandlers;
 
-    @Override
-    public HubEvent collectHubEvent(HubEvent event) {
-        log.info("Событие хаба: {}", event);
-        HubEventAvro hubEventAvro = EventMapper.toHubEventAvro(event);
-        producer.send(HUBS_TOPIC, hubEventAvro);
-        return event;
+    public EventServiceImpl(Set<SensorEventHandler> sensorEventHandlers, Set<HubEventHandler> hubEventHandlers) {
+        this.sensorEventHandlers = sensorEventHandlers.stream()
+                .collect(Collectors.toMap(
+                        SensorEventHandler::getMessageType,
+                        Function.identity()
+                ));
+        this.hubEventHandlers = hubEventHandlers.stream()
+                .collect(Collectors.toMap(
+                        HubEventHandler::getMessageType,
+                        Function.identity()
+                ));
     }
 
     @Override
-    public SensorEvent collectSensorEvent(SensorEvent event) {
-        log.info("Событие датчика: {}", event);
-        SensorEventAvro sensorEventAvro = EventMapper.toSensorEventAvro(event);
-        producer.send(SENSORS_TOPIC, sensorEventAvro);
-        return event;
+    public void collectHubEvent(HubEventProto event) {
+        try {
+            log.info("json: {}", JsonFormat.printer().print(event));
+        } catch (InvalidProtocolBufferException e) {
+            log.warn("Error occurred while processing HubEvent", e);
+        }
+
+        if (hubEventHandlers.containsKey(event.getPayloadCase())) {
+            hubEventHandlers.get(event.getPayloadCase()).handle(event);
+        } else {
+            throw new IllegalArgumentException("HubEventHandler not found for type: "
+                    + event.getPayloadCase());
+        }
+    }
+
+    @Override
+    public void collectSensorEvent(SensorEventProto event) {
+        try {
+            log.info("json: {}", JsonFormat.printer().print(event));
+        } catch (InvalidProtocolBufferException e) {
+            log.warn("Error occurred while processing SensorEvent", e);
+        }
+
+        if (sensorEventHandlers.containsKey(event.getPayloadCase())) {
+            sensorEventHandlers.get(event.getPayloadCase()).handle(event);
+        } else {
+            throw new IllegalArgumentException("SensorEventHandler not found for type: "
+                    + event.getPayloadCase());
+        }
     }
 }
